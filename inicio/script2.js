bf64601edadfa9b1f9ecf0c6bfe32bc1a771ab2c
migueljusto-2307 <justomiguel2307@gmail.com>
2026-08-28 15:42:46 -0300

    teste

--- inicio/script2.js
+++ inicio/script2.js
@@ -1,19 +1,43 @@
-const button = document.getElementById('button_card');
+const botoes = document.querySelectorAll('.button_card');
 
-const star_on = true;
+botoes.forEach(button => {
 
-const star = document.createElement('img');
-star.setAttribute('src', 'assets/icons/star=Padrão.png');
+    let star_on = true; 
 
-button.appendChild(star);
+    const star = document.createElement('img');
+    star.setAttribute('src', 'assets/icons/star=Padrão.png');
+    
+    button.insertBefore(star, button.firstChild);
 
+    star.addEventListener('click', () => {
+        if (star_on) {
+            star.setAttribute('src', 'assets/icons/star.png');
+            star_on = false;
+        } else {
+            star.setAttribute('src', 'assets/icons/star=Padrão.png');
+            star_on = true;
+        }
+    });
+});
 
-star.addEventListener('click', modific);
+const botao = document.querySelectorAll('.button-poke');
 
-function modific(){ 
+botao.forEach(button => {
 
+    let poke_on = true; 
 
-    star.setAttribute('src', 'assets/icons/star.png');
+    const poke = document.createElement('img');
+    poke.setAttribute('src', 'assets/icons/poke=Padrão.png');
+    
+    button.insertBefore(poke, button.firstChild);
 
-    button.appendChild(star);
-    };
+    poke.addEventListener('click', () => {
+        if (poke_on) {
+            poke.setAttribute('src', 'assets/icons/poke=hover.png');
+            poke_on = false;
+        } else {
+            poke.setAttribute('src', 'assets/icons/poke=Padrão.png');
+            poke_on = true;
+        }
+    });
+});
